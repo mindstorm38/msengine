c1caed4b48c4b2c4da98e77da61b1745da568007
Théo Rozier <contact@theorozier.fr>
2020-10-31 17:51:35 +0100

    Implemented "Object pool" concept, useful to avoid a lot of reallocation.

---
+++ common/src/main/java/io/msengine/common/util/pool/FixedObjectPool.java
@@ -0,0 +1,48 @@
+package io.msengine.common.util.pool;
+
+import java.util.ArrayDeque;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.function.Supplier;
+
+public class FixedObjectPool<T> extends ObjectPool<T> {
+	
+	private final Queue<PoolObject> pool = new ArrayDeque<>();
+	
+	public FixedObjectPool(Supplier<T> provider, int size) {
+		try {
+			for (int i = 0; i < size; ++i) {
+				this.pool.add(this.new PoolObject(provider.get()));
+			}
+		} catch (RuntimeException e) {
+			throw new IllegalArgumentException("The provider caused an exception.", e);
+		}
+	}
+	
+	@Override
+	protected PoolObject innerAcquire() throws NoSuchElementException {
+		if (this.pool.isEmpty()) {
+			throw new NoSuchElementException("No more object in this pool, be careful of objects leaks or increase the max count !");
+		}
+		return null;
+	}
+	
+	@Override
+	protected void innerRelease(PoolObject obj) {
+		this.pool.add(obj);
+	}
+	
+	@Override
+	public boolean hasMore() {
+		return !this.pool.isEmpty();
+	}
+	
+	public static <U> ObjectPool<U> newFixed(Supplier<U> provider, int size) {
+		return new FixedObjectPool<>(provider, size);
+	}
+	
+	public static <U> ObjectPool<U> newSyncFixed(Supplier<U> provider, int size) {
+		return new FixedObjectPool<>(provider, size).toSynchronized();
+	}
+	
+}
