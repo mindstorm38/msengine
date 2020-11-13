3818285b21207dcdf5fda549b395b849e6c4900c
Théo Rozier <contact@theorozier.fr>
2020-11-13 11:23:38 +0100

    feat(utils): Added PathUtils.isReservedPathName to check for windows reserved path names.

--- common/src/main/java/io/msengine/common/util/PathUtils.java
+++ common/src/main/java/io/msengine/common/util/PathUtils.java
@@ -2,8 +2,13 @@ package io.msengine.common.util;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class PathUtils {
+	
+	private static Set<String> RESERVED_FILENAMES;
 	
 	public static Path getAppDataPath() {
 		
@@ -27,6 +32,17 @@ public final class PathUtils {
 			return Paths.get(path);
 		}
 		
+	}
+	
+	public static boolean isReservedPathName(String name) {
+		if (RESERVED_FILENAMES == null) {
+			RESERVED_FILENAMES = new HashSet<>(Arrays.asList(
+					"COM", "PRN", "AUX", "NUL",
+					"COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+					"LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"
+			));
+		}
+		return RESERVED_FILENAMES.contains(name.toUpperCase());
 	}
 	
 	private PathUtils() { }
